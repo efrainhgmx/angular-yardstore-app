4fcc6a9f7f817480d2e68858ac1320d131778485
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-18 17:35:30 -0500

    render of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'yard-store';
   barProgress = 50;
+
+  products: Product[] = [
+    {
+      name: 'Tea Cup',
+      price: 7.50,
+      image: '../assets/images/cup.webp'
+    },
+    {
+      name: 'Large Desk for work',
+      price: 150,
+      image: '../assets/images/desk.webp'
+    },
+    {
+      name: 'Glasses',
+      price: 64.99,
+      image: '../assets/images/glasses.webp'
+    },
+    {
+      name: 'MacBook Pro M2',
+      price: 1750,
+      image: '../assets/images/macbook.webp'
+    },
+    {
+      name: 'Red Wine',
+      price: 147.99,
+      image: '../assets/images/wine.webp'
+    },
+    {
+      name: 'iPhone XS',
+      price: 800,
+      image: '../assets/images/phone.webp'
+    },
+  ]
 }
